833cd8de1c7e992a6c05cf1c3268d7cf310fda34
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-04-10 12:01:05 -0700

    WEB502 HWK M4 W2 D1 initiate

---
+++ index.js
@@ -0,0 +1,78 @@
+/* Express Setup */
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+const bodyParser = require('body-parser');
+const expressSession = require('express-session') ({
+    secret:'secret',
+    resave: false,
+    saveUninitialized:false
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSession);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ' + port));
+
+/* Passport Setup */
+const passport = require('passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* Mongoose Setup */
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect('mongodb://localhost/MyDatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+    username: String,
+    password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+/* Passport Local Authentication */
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+/* Routes */
+const connectEnsureLogin = require('connect-ensure-login');
+const { redirect } = require('express/lib/response');
+
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', 
+    (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect('/login?info=' + info);
+        }
+
+        req.logIn(user, function(err) {
+            if (err) {
+                return next(err);
+            }
+
+            return res.redirect('/');
+        });
+    }) (req, res, next);
+});
+
+
+
+
+
